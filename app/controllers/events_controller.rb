583f14052faa5bf36f552c8ec5a3d6625ce3a1b0
Gregory Doran <greg@gregorydoran.co.uk>
2015-09-30 22:32:11 +0100

    Made requester have to specify json as the encoding of the input

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,16 @@ class EventsController < ApplicationController
   skip_before_action :verify_authenticity_token, if: :json_request?, only: [ :record ]
 
   def record
-    event = Event.from_json(request.body.read)
-    if event
-      event.save!
-      render nothing: true, status: 200
-    else
-      render nothing: true, status: 400
+    respond_to do |format|
+      format.json do
+        event = Event.from_json(request.body.read)
+        if event
+          event.save!
+          render nothing: true, status: 200
+        else
+          render nothing: true, status: 400
+        end
+      end
     end
   end
 
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -8,21 +8,21 @@ class EventsControllerTest < ActionController::TestCase
 
   test 'record: valid JSON' do
     assert_difference 'Event.count', 1 do
-      post :record, '{"Address":"barney@lostmy.name","EmailType":"Shipment","Event":"send","Timestamp":1432820696}'
+      post :record, '{"Address":"barney@lostmy.name","EmailType":"Shipment","Event":"send","Timestamp":1432820696}', format: :json
     end
     assert_response :success
   end
 
   test 'record: invalid JSON' do
     assert_no_difference 'Event.count' do
-      post :record, '{"Address":"barney@lostmy.name","EmailType":"Shipment","Event":"send","Timestamp":1432820696'
+      post :record, '{"Address":"barney@lostmy.name","EmailType":"Shipment","Event":"send","Timestamp":1432820696', format: :json
     end
     assert_response :bad_request
   end
 
   test 'record: missing property' do
     assert_no_difference 'Event.count' do
-      post :record, '{"Address":"barney@lostmy.name","Timestamp":1432820696}'
+      post :record, '{"Address":"barney@lostmy.name","Timestamp":1432820696}', format: :json
     end
     assert_response :bad_request
   end
